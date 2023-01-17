193a690369251d8bff29a57163df725d40caa784
sun <982268620@qq.com>
2023-01-17 11:38:05 +0800

    【提交版本】V0【提交内容】配置eslint和prettier，引入antd组件库

--- static/src/App.vue
+++ static/src/App.vue
@@ -1,85 +1,45 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import { RouterLink, RouterView } from 'vue-router';
+const selectedKeys = ref<string[]>(['2']);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <a-layout>
+        <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
+            <div class="logo" />
+            <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+                <a-menu-item key="1">nav 1</a-menu-item>
+                <a-menu-item key="2">nav 2</a-menu-item>
+                <a-menu-item key="3">nav 3</a-menu-item>
+            </a-menu>
+        </a-layout-header>
+        <a-layout-content :style="{ padding: '0 50px', marginTop: '64px' }">
+            <a-breadcrumb :style="{ margin: '16px 0' }">
+                <a-breadcrumb-item>Home</a-breadcrumb-item>
+                <a-breadcrumb-item>List</a-breadcrumb-item>
+                <a-breadcrumb-item>App</a-breadcrumb-item>
+            </a-breadcrumb>
+            <div :style="{ background: '#fff', padding: '24px', minHeight: '380px' }">
+                Content
+            </div>
+        </a-layout-content>
+    </a-layout>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+#components-layout-demo-fixed .logo {
+    width: 120px;
+    height: 31px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px 24px 16px 0;
+    float: left;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.site-layout .site-layout-background {
+    background: #fff;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+[data-theme='dark'] .site-layout .site-layout-background {
+    background: #141414;
 }
 </style>
--- static/src/main.ts
+++ static/src/main.ts
@@ -1,14 +1,17 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-import './assets/main.css'
+import 'ant-design-vue/dist/antd.css';
+import './assets/main.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
+app.use(Antd);
 
-app.mount('#app')
+app.mount('#app');
